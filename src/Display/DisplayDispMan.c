ff47a1cac288523d7bebbae8c60bcac5b0817421
manu88 <manuel.deneu@gmail.com>
2017-05-20 12:12:41 +0200

    Added keyboard modifiers

--- src/Display/DisplayDispMan.c
+++ src/Display/DisplayDispMan.c
@@ -347,6 +347,7 @@ unsigned version;
 
 static int getKey( const  Display* disp )
 {
+    static GXKeyMod mods = (GXKeyMod) 0;
     struct input_event ev[EV_BUF_SIZE]; /* Read up to N events ata time */
     int  sz;
     unsigned i;
@@ -391,14 +392,26 @@ static int getKey( const  Display* disp )
                 
                 printf("Code : %02x\n" , ev[i].code);
                 */
-
+                
+                if( ev[i].code == KEY_RIGHTSHIFT || ev[i].code == KEY_LEFTSHIFT )
+                {
+                    if( ev[i].value == 1)
+                    {
+                        mods |= GXKeyMod_SHIFT;
+                    }
+                    else if( ev[i].value == 0)
+                    {
+                        mods &= GXKeyMod_SHIFT;
+                    }
+                }
+                
                 
                  GXEventKey keyEv;
                  keyEv.type = GXEventTypeKey;
                  
                  keyEv.action   = (GXKeyAction)ev[i].value;
                  keyEv.code     = KeyConvToGX( ev[i].code );
-                 keyEv.mod      = (GXKeyMod)0;
+                 keyEv.mod      = mods;
                  keyEv.scanCode = 0;
                  
                  disp->eventListener((void*)disp , (const GXEvent*) &keyEv);
@@ -598,6 +611,8 @@ static GXKeyCode KeyConvToGX( int k)
         default:
             break;
     }
+    
+    printf("Non mapped Key : code %i\n" , k);
     return (GXKeyCode)0;
 }
 
